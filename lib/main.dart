fcad6b06f5faafbe0c3ece54b5db66879de40904
DANIEL-DEMELASH <danieldemelash5@gmail.com>
2022-09-24 11:04:58 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:facebook_responsive_ui/config/palette.dart';
+import 'package:facebook_responsive_ui/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FaceBook Flutter UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          scaffoldBackgroundColor: Palette.scaffold),
+      home: const HomeScreen(),
+    );
+  }
+}
